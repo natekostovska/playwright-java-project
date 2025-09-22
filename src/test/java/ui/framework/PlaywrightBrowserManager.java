529d1c3bb6eef96aeab87eaa42461abd40891f86
Natasha Kostovska <nate_kostovska_4@hotmail.com>
2025-09-22 16:25:48 +0200

    Resolving issues

--- src/test/java/ui/framework/PlaywrightBrowserManager.java
+++ src/test/java/ui/framework/PlaywrightBrowserManager.java
@@ -2,7 +2,6 @@ package ui.framework;
 
 import com.microsoft.playwright.*;
 import lombok.Getter;
-
 import java.util.Arrays;
 
 @Getter
@@ -19,13 +18,7 @@ public class PlaywrightBrowserManager {
             case "chrome":
                 browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
                         .setHeadless(headless)
-                        .setArgs(Arrays.asList(
-                                "--disable-extensions",
-                                "--start-maximized",
-                                "--disable-blink-features=AutomationControlled",
-                                "--no-sandbox",
-                                "--disable-dev-shm-usage"
-                        )));
+                        .setArgs(Arrays.asList("--disable-extensions", "--start-maximized")));
                 break;
             case "firefox":
                 browser = playwright.firefox().launch(new BrowserType.LaunchOptions()
@@ -39,28 +32,9 @@ public class PlaywrightBrowserManager {
                 throw new IllegalArgumentException("Unsupported browser: " + browserName);
         }
 
-        context = browser.newContext(new Browser.NewContextOptions()
-                .setViewportSize(null)
-        );
+        context = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
 
         page = context.newPage();
-
-        // Inject stealth scripts BEFORE any navigation
-        page.addInitScript("""
-            Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-
-            const originalQuery = window.navigator.permissions.query;
-            window.navigator.permissions.query = (parameters) => (
-                parameters.name === 'notifications' ?
-                Promise.resolve({ state: Notification.permission }) :
-                originalQuery(parameters)
-            );
-
-            Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
-
-            Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-        """);
-
         return page;
     }
 
@@ -74,4 +48,5 @@ public class PlaywrightBrowserManager {
         if (browser != null) browser.close();
         if (playwright != null) playwright.close();
     }
+
 }
